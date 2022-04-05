e8d1f04f00f8c878f24105e9f2bff4968f2d0c00
edward2mv <edward.emv@gmail.com>
2022-04-05 19:25:54 -0400

    adding  on event change handler in search compoent

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,20 @@
 import styles from './Search.module.css'
 import {FaSearch} from "react-icons/fa";
+import {useState} from "react";
 
 export default function Search() {
+  const [searchText, setSearchText] = useState("");
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+  }
+  console.log(searchText);
   return (
-    <form className={styles.searchContainer}>
+    <form className={styles.searchContainer} onSubmit={handleSubmit}>
     <div className={styles.searchBox}>
-        <input className={styles.searchInput} type="text"/>
+        <input className={styles.searchInput} type="text"
+        value={searchText}
+        onChange={(e)=> setSearchText(e.target.value)}
+        />
         <button className={styles.searchButton} type="submit">
           <FaSearch size ={20}/>
         </button>
