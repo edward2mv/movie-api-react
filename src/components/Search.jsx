81887cb866f793cbcfbb80c192823bdc34f794f9
edward2mv <edward.emv@gmail.com>
2022-04-05 18:53:56 -0400

    styles  in the search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,14 @@
-
+import styles from './Search.module.css'
+import {FaSearch} from "react-icons/fa";
 
 export default function Search() {
   return (
-    <form className="searchContainer">
-    <div className="">
-        <input type="text"/>
-        <button type="submit">buscar</button>
+    <form className={styles.searchContainer}>
+    <div className={styles.searchBox}>
+        <input className={styles.searchInput} type="text"/>
+        <button className={styles.searchButton} type="submit">
+          <FaSearch size ={20}/>
+        </button>
     </div>
     </form>
   )
