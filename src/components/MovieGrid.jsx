b626a7574af7ee7aa135f3cf9ee43e3462b2044c
edward2mv <edward.emv@gmail.com>
2022-04-05 21:38:48 -0400

    commit

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -1,4 +1,6 @@
 import {useEffect, React,useState} from 'react'
+
+import { useQuery } from '../hooks/useQuery.jsx';
 import { get } from '../utils/httpClient.js';
 import { Loader } from './Loader.jsx';
 import MovieCard from './MovieCard.jsx'
@@ -8,13 +10,21 @@ import styles from './MovieGrid.module.css';
 function MovieGrid() {
 const [movies,setMovies] = useState([]);
 const [isLoading, setIsLoading] = useState(true);
-    useEffect(() =>{
+
+const query = useQuery()
+const search = query.get("search");
+
+
+    useEffect(() => {
     setIsLoading(true);
-    get("/discover/movie").then(data =>{
+    const searchUrl = search
+    ? "/search/movie?query="+ search
+    : "/discover/movie";
+    get(searchUrl).then(data =>{
             setMovies(data.results);
         setIsLoading(false);
         })
-    },[]);
+    },[search]);
 
     if (isLoading){
         return <Loader/>
