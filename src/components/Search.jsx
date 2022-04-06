651a1e9e08052358c3d8fc36a973ca17a396aadc
edward2mv <edward.emv@gmail.com>
2022-04-05 20:28:46 -0400

    using useNavigate from react-router

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,24 @@
 import styles from './Search.module.css'
 import {FaSearch} from "react-icons/fa";
 import {useState} from "react";
+import { useNavigate } from 'react-router-dom';
 
 export default function Search() {
   const [searchText, setSearchText] = useState("");
+  const history = useNavigate();
   const handleSubmit = (e) =>{
     e.preventDefault();
+    history("/?search=" +searchText);
   }
   console.log(searchText);
   return (
-    <form className={styles.searchContainer} onSubmit={handleSubmit}>
+    <form className={styles.searchContainer}
+      onSubmit={handleSubmit}
+    >
     <div className={styles.searchBox}>
         <input className={styles.searchInput} type="text"
         value={searchText}
-        onChange={(e)=> setSearchText(e.target.value)}
+        onChange= {(e)=> setSearchText(e.target.value)}
         />
         <button className={styles.searchButton} type="submit">
           <FaSearch size ={20}/>
