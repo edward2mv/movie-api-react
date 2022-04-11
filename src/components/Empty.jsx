fd70ad754da54eb4734c6e9f31ffe4a1f6ca0dbb
edward2mv <edward.emv@gmail.com>
2022-04-10 20:30:50 -0400

    commit del commit porque no recuerdo si ya hice commit

---
+++ src/components/Empty.jsx
@@ -0,0 +1,8 @@
+import styles from "./Empty.module.css";
+export function Empty(){
+    return (
+        <div className={styles.empty}>
+            no results found
+        </div>
+    )
+}
---
+++ src/utils/getMovieImg.js
@@ -0,0 +1,6 @@
+import placeholder from "../placeholder.jpg"
+
+export function getMovieImg(path, width){
+    return path ? `https://image.tmdb.org/t/p/w${width}${path}` : placeholder;
+
+}
