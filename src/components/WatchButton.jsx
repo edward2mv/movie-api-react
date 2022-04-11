42c12bcbec0fe18bbd7c8f1d1fb661fc0aa508ce
edward2mv <edward.emv@gmail.com>
2022-04-11 19:15:45 -0400

    className hotfix

--- src/components/WatchButton.jsx
+++ src/components/WatchButton.jsx
@@ -3,7 +3,7 @@ export function WatchButton(props){
     return (
         <div className={styles.wrapper}>
             <div className={styles.outer}>
-                <button onClick={()=>window.open(props.movieHomepage, '_blank')}>Watch Now</button>
+                <button className={styles.watchButton} onClick={()=>window.open(props.movieHomepage, '_blank')}>Watch Now</button>
                 <span></span>
                 <span></span>
             </div>
